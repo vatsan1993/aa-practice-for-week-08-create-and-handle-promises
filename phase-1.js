bcd5097313655dde554917c76b6a45575ab51b6f
Srivatsan <vatsan1993@gmail.com>
2024-05-31 13:35:51 +0530

    finished project

--- phase-1.js
+++ phase-1.js
@@ -1,19 +1,49 @@
 function stretch() {
   // Your code here
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, 1000);
+  });
 }
 
 function runOnTreadmill() {
   // Your code here
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, 500);
+  });
 }
 
 function liftWeights() {
   // Your code here
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, 2000);
+  });
 }
 
 function workout() {
   // Your code here
+  let stretchPromise = stretch();
+  stretchPromise
+    .then(() => {
+      console.log('done stretching');
+    })
+    .then(runOnTreadmill)
+    .then(() => {
+      console.log('done running on treadmill');
+    })
+    .then(liftWeights)
+    .then(() => {
+      console.log('done lifting weights');
+    })
+    .then(() => {
+      console.log('done working out');
+    });
 }
-
 
 /* ============================ TEST YOUR CODE ============================
 
@@ -21,10 +51,9 @@ Run the file (`node phase-1.js`) and check your output against the expected
 output.
 */
 
-
 workout();
-  // should print out the following:
-    // done stretching
-    // done running on treadmill
-    // done lifting weights
-    // done working out
+// should print out the following:
+// done stretching
+// done running on treadmill
+// done lifting weights
+// done working out
--- phase-2.js
+++ phase-2.js
@@ -1,24 +1,77 @@
 function stretch(timeLeft) {
   // refactor your code from phase 1
   // Your code here
+  return new Promise((resolve, reject) => {
+    let timeNeeded = 1000;
+    if (timeLeft >= timeNeeded) {
+      setTimeout(() => {
+        resolve(timeNeeded);
+      }, timeNeeded);
+    } else {
+      reject('you dont have enough time to stretch');
+    }
+  });
 }
-
 
 function runOnTreadmill(timeLeft) {
   // refactor your code from phase 1
   // Your code here
-}
+  let timeNeeded = 500;
 
+  return new Promise((resolve, reject) => {
+    if (timeLeft >= timeNeeded) {
+      setTimeout(() => {
+        resolve(timeNeeded);
+      }, timeNeeded);
+    } else {
+      reject('you dont have enough time to run on treadmill');
+    }
+  });
+}
 
 function liftWeights(timeLeft) {
   // refactor your code from phase 1
   // Your code here
+  let timeNeeded = 2000;
+  return new Promise((resolve, reject) => {
+    if (timeLeft >= timeNeeded) {
+      setTimeout(() => {
+        resolve(timeNeeded);
+      }, timeNeeded);
+    } else {
+      reject('you dont have enough time to lift weights');
+    }
+  });
 }
-
 
 function workout(totalTime) {
   // refactor your code from phase 1
   // Your code here
+  let reduceTime = (timeSpent) => {
+    totalTime -= timeSpent;
+  };
+  let stretchPromise = stretch(totalTime);
+  stretchPromise
+    .then(reduceTime)
+    .then(() => {
+      console.log('Done Stretch');
+    })
+    .then(() => runOnTreadmill(totalTime))
+    .then(reduceTime)
+    .then(() => {
+      console.log('Done Threadmill');
+    })
+    .then(() => liftWeights(totalTime))
+    .then(reduceTime)
+    .then(() => {
+      console.log('Done Weights');
+    })
+    .then(() => {
+      console.log(`Done Workout with ${totalTime / 1000} seconds`);
+    })
+    .catch((message) => {
+      console.log('Error: ' + message);
+    });
 }
 
 /* ============================ TEST YOUR CODE ============================
@@ -27,28 +80,24 @@ Comment in each invocation of your workout function below and run the file
 (node phase-2.js) to see if you get the expected output.
 */
 
-
 // workout(500);
-  // should print out the following:
-    // Error:  you dont have enough time to stretch
-
+// should print out the following:
+// Error:  you dont have enough time to stretch
 
 // workout(1000);
-  // should print out the following:
-    // done stretching
-    // Error:  you dont have enough time to run on treadmill
-
+// should print out the following:
+// done stretching
+// Error:  you dont have enough time to run on treadmill
 
 // workout(2000);
-  // should print out the following:
-    // done stretching
-    // done running on treadmill
-    // Error:  you dont have enough time to lift weights
+// should print out the following:
+// done stretching
+// done running on treadmill
+// Error:  you dont have enough time to lift weights
 
-
-// workout(4000);
-  // should print out the following:
-  //   done stretching
-  //   done running on treadmill
-  //   done lifting weights
-  //   done working out with 0.5 seconds left
+workout(4000);
+// should print out the following:
+//   done stretching
+//   done running on treadmill
+//   done lifting weights
+//   done working out with 0.5 seconds left
